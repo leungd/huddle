36905c6687607f14522a9070f53c64e1b4aebafa
Derek Leung <me@derekleung.ca>
2014-08-08 15:56:24 -0400

    create profile model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+	has_one :profile, dependent: :destroy
+
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'profiles/index'
+
+  get 'profiles/show'
+
   root to: 'visitors#index'
   devise_for :users
   resources :users
